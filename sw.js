a25f5da254242fe22fa4926aef6da3e1432cadf0
Ailen Laguda <ailenlaguda@Ai-MacBook-Pro.local>
2026-02-10 16:10:04 +0800

    initial commit

--- sw.js
+++ sw.js
@@ -1,10 +1,34 @@
-const CACHE_NAME = 'ssu-tracker-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'ssu-tracker-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// Install Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activate & Cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// Fetch events
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
